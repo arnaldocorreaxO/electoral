3d96d55c312d9a3d5752b8168e4659d183cc6500
arnaldocorreaxO <arnalgo.guillermo@gmail.com>
2021-04-18 00:46:05 +0000

    .gitignore fix

--- app/core/electoral/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ElectoralConfig(AppConfig):
-    name = 'electoral'
--- app/core/login/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-admin.site.site_header = 'Sistema Electoral'
-admin.site.site_title = ' Portal Sistema Electoral'
-admin.site.index_title = 'Bienvenido al Sistema Electoral'
