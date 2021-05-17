d2bf429dd0605160eeb3109da1e5e7c5faf6fce3
arnaldocorreaxO <arnaldo.guillermo@gmail.com>
2021-05-17 11:30:35 -0400

    Change CM

--- app/core/electoral/tests.py
+++ app/core/electoral/tests.py
@@ -1,3 +1,63 @@
+
+import os
+
+
 from django.test import TestCase
+from pyreportjasper import PyReportJasper
+
 
 # Create your tests here.
+def processing():
+	REPORTS_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)+ "/../"), 'reports')
+	print(REPORTS_DIR)
+	# JDBC_DIR = os.path.join(REPORTS_DIR,'sqlite-jdbc-3.34.0.jar')
+	JDBC_DIR = os.path.join(REPORTS_DIR,'postgresql-42.2.20.jar')
+	SQLITE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'db.sqlite3')
+	print(JDBC_DIR)
+	print('jdbc:sqlite:'+SQLITE_DIR)
+	input_file = os.path.join(REPORTS_DIR, 'rpt_001.jrxml')
+	output_file = os.path.join(REPORTS_DIR, 'rpt_001')
+	pyreportjasper = PyReportJasper()
+	# conn = {
+	#         'driver': 'sqlite3',
+	#         'jdbc_driver': 'org.sqlite.JDBC',
+	#         # 'url': 'jdbc:sqlite:'+SQLITE_DIR,
+	#         'jdbc_dir': JDBC_DIR,
+	# 		'database': 'jdbc:sqlite:C:\\Users\\arnaldo\\proyectos\\electoral\\app\\db\\db.sqlite3',
+	# }
+	conn = {
+			'driver': 'postgres',
+			'username': 'postgres',
+			'password': 'ox82',
+			'host': 'localhost',
+			'database': 'electoral',
+			'schema': 'public',
+			'port': '5432',
+			'jdbc_driver':'org.postgresql.Driver',
+			'jdbc_dir': JDBC_DIR
+	}
+	print(input_file)	
+
+	params = {
+            'P_BARRIO_ID': 2,
+            'P_MANZANA_COD': None,
+            'P_TITULO1': 'ASOCIACION NACIONAL REPUBLICANA',
+            'P_TITULO2': '"PARTIDO COLORADO"',
+            'P_REPORTE': 'MOVIMIENTO LISTA 6 "JOAQUINA INTENDENTA"',
+            'P_RUTA': REPORTS_DIR,
+        }
+
+	print(params)
+	pyreportjasper.config(
+			input_file,
+			output_file,
+			db_connection=conn,			
+			output_formats=["pdf", "xls"],
+			parameters=params,
+			locale='es_PY'
+	)
+	# output = pyreportjasper.list_report_params()
+	# print(output)
+	pyreportjasper.process_report()
+
+processing()
