31bdc14500daa8d6d1ce6914744c6fc61e3ce8ca
arnaldocorreaxO <arnaldo.guillermo@gmail.com>
2021-06-19 16:48:20 -0400

    Change Electorl

--- app/core/electoral/admin.py
+++ app/core/electoral/admin.py
@@ -112,7 +112,7 @@ class ElectorAdmin3(ImportExportModelAdmin):
     resource_class = ElectorResources3 
     form = ElectorForm2 
     readonly_fields = ('ci','nombre','apellido','edad')   
-    list_display =['ci','nombre','apellido','edad','tipo_voto','manzana','nro_casa']
+    list_display =['orden','ci','nombre','apellido','edad','tipo_voto','manzana','nro_casa']
     list_editable =['tipo_voto','nro_casa'] #Consume muchos recursos (tarda mucho la consulta)
     list_filter =['ciudad','seccional','barrio','manzana','tipo_voto','mesa']
     search_fields =['ci','nombre','apellido']
