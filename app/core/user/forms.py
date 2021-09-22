194d55e3ad759c3d3f5c5547bd0bb62732394d25
arnaldocorreaxO <arnaldo.guillermo@gmail.com>
2021-09-22 17:58:05 -0400

    Change User

--- app/core/user/forms.py
+++ app/core/user/forms.py
@@ -13,7 +13,7 @@ class UserForm(ModelForm):
 
     class Meta:
         model = User
-        fields = 'first_name', 'last_name', 'username', 'password', 'dni', 'email', 'groups', 'image'
+        fields = 'first_name', 'last_name', 'username', 'password', 'dni', 'email', 'groups', 'image','is_active', 'is_staff', 'is_superuser'
         widgets = {
             'first_name': forms.TextInput(attrs={'placeholder': 'Ingrese sus nombres'}),
             'last_name': forms.TextInput(attrs={'placeholder': 'Ingrese sus apellidos'}),
@@ -22,6 +22,7 @@ class UserForm(ModelForm):
             'email': forms.TextInput(attrs={'placeholder': 'Ingrese su correo electrónico'}),
             'password': forms.PasswordInput(render_value=True, attrs={'placeholder': 'Ingrese un password'}),
             'groups': forms.SelectMultiple(attrs={'class': 'select2', 'multiple': 'multiple', 'style': 'width:100%'}),
+    
         }
         exclude = ['is_change_password', 'user_permissions', 'date_joined',
                    'last_login', 'token']
